3f9d774ab272e4eff1ee116e8120504d82e231a7
Advik01-byte <iamadvikgoyal@gmail.com>
2026-03-27 18:23:57 +0530

    Update intermediate.py

--- exercises/python/intermediate.py
+++ exercises/python/intermediate.py
@@ -1,9 +1,1 @@
-# Intermediate Python Exercise
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-print(factorial(5))
+# Write a function to find the factorial of a number
